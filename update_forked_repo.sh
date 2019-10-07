dd2b0be385c021257aab3b111bfa3ef8481c7824
Sharad Agarwal <sharad.agarwal2014@vit.ac.in>
2019-10-07 16:07:51 +0200

    Update update_forked_repo.sh

--- update_forked_repo.sh
+++ update_forked_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export PATH=$PATH:/home/sharad/Desktop/WmAgentScripts
+export PATH=$PATH:<provide path to the repo>
 git fetch original #check if original is setup using git remote -v
 git checkout master
 git rebase original/master
